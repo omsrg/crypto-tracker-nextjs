3e09c42878de52d3ae379b1cb7eeb3b68d85f092
omsrg <siregar.om@gmail.com>
2022-02-20 18:59:00 +0700

    configured api-utils

--- src/helpers/api-utils.ts
+++ src/helpers/api-utils.ts
@@ -1,5 +1,21 @@
 // import { TrendingCoins } from './api';
 import { CurrencyType } from '@/types/types';
+import { useQuery } from 'react-query';
+
+export const fetchCoinList = async (currency: CurrencyType) => {
+	const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+
+	const response = await fetch(URL);
+	if (!response.ok) {
+		throw new Error('Error Fetching Data!');
+	}
+
+	return await response.json();
+};
+
+export const useCoinList = (currency: CurrencyType) => {
+	return useQuery(['coinList', currency], () => fetchCoinList(currency));
+};
 
 export const fetchTrendingCoins = async (currency: CurrencyType) => {
 	const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
@@ -10,4 +26,23 @@ export const fetchTrendingCoins = async (currency: CurrencyType) => {
 	}
 
 	return await response.json();
+};
+
+export const useTrendingCoins = (currency: CurrencyType) => {
+	return useQuery(['trendingCoins', currency], () => fetchTrendingCoins(currency));
+};
+
+export const fetchSingleCoin = async (id: string) => {
+	const URL = `https://api.coingecko.com/api/v3/coins/${id}`;
+
+	const response = await fetch(URL);
+	if (!response.ok) {
+		throw new Error('Error Fetching Data!');
+	}
+
+	return await response.json();
+};
+
+export const useSingleCoin = (id: string) => {
+	return useQuery(['singleCoin', id], () => fetchSingleCoin(id));
 };
--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,10 +1,9 @@
 import { CurrencyType } from '@/types/types';
 
-// export const CoinList = (currency) =>
-//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+export const CoinList = (currency: CurrencyType) =>
+	`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
-// export const SingleCoin = (id) =>
-//   `https://api.coingecko.com/api/v3/coins/${id}`;
+export const SingleCoin = (id: number) => `https://api.coingecko.com/api/v3/coins/${id}`;
 
 // export const HistoricalChart = (id, days = 365, currency) =>
 //   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
